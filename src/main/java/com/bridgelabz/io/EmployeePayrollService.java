a93ba293f1c0ceac34ff67ce085c19c044c3436e
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-20 15:40:17 +0530

    Added:methods to read and write from console.

--- src/main/java/com/bridgelabz/io/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/io/EmployeePayrollService.java
@@ -1,9 +1,34 @@
 package com.bridgelabz.io;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class EmployeePayrollService
 {
-    public static void main(String[] args)
+    private List<EmployeePayrollData> employeePayrollList;
+    public EmployeePayrollService(ArrayList<EmployeePayrollData> employeePayrollList)
     {
-        System.out.println("Welcome to Employee Payroll Service");
+        this.employeePayrollList = employeePayrollList;
+    }
+    public static void main(String[] args) {
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+    }
+    private void readEmployeePayrollData(Scanner consoleInputReader)
+    {
+        System.out.println("Enter Employee ID: ");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Employee Name: ");
+        String name = consoleInputReader.next();
+        System.out.println("Enter Employee salary: ");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id,name,salary));
+    }
+    private void writeEmployeePayrollData()
+    {
+        System.out.println("\n Writing Employee Payroll Roaster to Console\n"+employeePayrollList);
     }
 }
